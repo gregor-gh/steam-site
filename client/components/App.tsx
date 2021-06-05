f43802b8fc7a18bbc4cf6627661cc3db0f4ac8c4
Gregor Imrie <gregor@gregors-air.home>
2021-06-05 15:14:30 +0100

    Added test button to authenticate with Steam

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,16 @@
 import "../styles/App.css";
+import { useEffect } from "react";
+import axios from "axios"
+
+const test = async () => {
+  window.location.href="/api/auth"
+}
+
 
 const App = (): JSX.Element => (
   <>
     <h1>React TypeScript Webpack Starter Template {process.env.name}</h1>
+    <button onClick={test}>test</button>
   </>
 );
 
