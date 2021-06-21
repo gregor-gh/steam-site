6561bce317ad0882c9f218d9d1b13b29f89826e8
Gregor <73136777+gregor-gh@users.noreply.github.com>
2021-06-22 00:07:48 +0100

    Added SteamSpyGameList type

---
+++ @types/steamSpy.d.ts
@@ -0,0 +1,19 @@
+interface SteamSpyGameList {
+  appid: number;
+  average_2weeks: number;
+  average_forever: number;
+  ccu: number;
+  developer: string;
+  discount: string;
+  initialprice: string;
+  median_2weeks: number;
+  median_forever: number;
+  name: string;
+  negative: number;
+  owners: string;
+  positive: number;
+  price: string;
+  publisher: string;
+  score_rank: string;
+  userscore: number;
+}
