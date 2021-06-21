7fd80a5e548c0144d94c9a4b75b6d4cbd70e2fe1
Gregor <73136777+gregor-gh@users.noreply.github.com>
2021-06-22 00:30:27 +0100

    Pulling through top 10 games

--- client/components/TopGames/TopGames.tsx
+++ client/components/TopGames/TopGames.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const gameList = ["test", "test2", "test3", "test4", "test5"];
+//const gameList = ["test", "test2", "test3", "test4", "test5"];
+
+async function fetchTopGames() {}
 
 export const TopGames = () => {
+  const [gameList, setGameList] = useState<SteamSpyGameList[]>([]);
+
+  useEffect(() => {
+    fetch("/api/steam/top-games-two-weeks").then((response) =>
+      response.json().then((data) => setGameList(data))
+    );
+  }, []);
+
   return (
     <ol>
-      {gameList.map((game, index) => {
-        return <li key={index}>{game}</li>
+      {gameList.splice(0, 10).map((game, index) => {
+        return <li key={index}>{game.name}</li>;
       })}
     </ol>
   );
