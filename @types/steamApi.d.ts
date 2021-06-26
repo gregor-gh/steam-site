7aea6bf2bc9d518ab37c32f812399e782a0a9782
Gregor <73136777+gregor-gh@users.noreply.github.com>
2021-06-27 00:46:59 +0100

    Added steam news API interface

---
+++ @types/steamApi.d.ts
@@ -0,0 +1,21 @@
+interface SteamNewsItem {
+  gid: string;
+  title: string;
+  url: string;
+  is_external_url: boolean;
+  author: string;
+  contents: string;
+  feedlabel: string;
+  date: date;
+  feedname: string;
+  feed_type: number;
+  appid: number;
+}
+
+interface SteamGetNewsForApp {
+  appid: number;
+  newsitems: NewsItem[];
+  count: number;
+}
+
+type SteamResponseFormat = "json" | "xml" | "vdf";
