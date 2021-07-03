6460d923ba3f36dea2be28b7e0c5ed94cce7956d
Gregor <73136777+gregor-gh@users.noreply.github.com>
2021-07-03 12:51:35 +0100

    Remove steam clan image from news text

--- client/helpers/text.ts
+++ client/helpers/text.ts
@@ -1,5 +1,5 @@
 export function stripHtmlFromString(content: string) {
-  const document = new DOMParser().parseFromString(content, "text/html");
-  const textContent = document.body.textContent || "";
-  return textContent.replace(/\S*.jpg/, "");
+  const document = new DOMParser().parseFromString(content, "text/html"); // strip out html
+  const textContent = document.body.textContent || ""; // replace with empty string if requried
+  return textContent.replace(/\S*.jpg|\S*.png|{\S*/g, ""); // remove images/steam clan links
 }
