652c0e092de91b6088092afdfa9e433b3316da44
Gregor <73136777+gregor-gh@users.noreply.github.com>
2021-07-03 12:21:10 +0100

    Added fontawesome profile button

--- client/components/Navbar/Navbar.tsx
+++ client/components/Navbar/Navbar.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import "./Navbar.css";
 
 export const Navbar = () => {
   return (
     <header className="header">
       <nav className="navbar">
-        <div className="navbar-item"><img className="navbar-logo" src="https://upload.wikimedia.org/wikipedia/commons/8/87/New_Steam_Logo_with_name.jpg"/></div>
-        <div className="navbar-item">😀</div>
+        <div className="navbar-item">
+          <img
+            className="navbar-logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/8/87/New_Steam_Logo_with_name.jpg"
+          />
+        </div>
+        <FontAwesomeIcon
+          className="navbar-item navbar-profile"
+          icon={faUserCircle}
+          size="2x"
+          onMouseOver={(e) => {
+            
+          }}
+        />
       </nav>
     </header>
   );
