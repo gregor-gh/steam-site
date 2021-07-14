10eec397c6a2a9be2b7b7bf0109c8937963ea202
Gregor Imrie <73136777+gregor-gh@users.noreply.github.com>
2021-07-14 10:20:07 +0100

    Added db user type

--- @types/database.d.ts
+++ @types/database.d.ts
@@ -3,3 +3,10 @@ import { ISqlType, ISqlTypeFactoryWithNoParams, ISqlTypeWithLength, ISqlTypeWith
 type SqlColumnTypes = ISqlTypeFactoryWithNoParams | ISqlTypeWithNoParams
 type TableCreationColumn = [string, SqlColumnTypes, boolean];
 type TableCreation = TableCreationColumn[];
+
+interface DbSteamUser {
+  id: number;
+  steamId: string;
+  displayName: string;
+  photoUrl: string;
+}
