c987a19de081de0764bded351acf49fcdab03d06
Gregor Imrie <73136777+gregor-gh@users.noreply.github.com>
2021-07-14 10:20:24 +0100

    Added initial register process on strat

--- server/strategies/steamStrategy.ts
+++ server/strategies/steamStrategy.ts
@@ -1,20 +1,24 @@
-import config from "../config"
+import config from "../config";
+import { steamCreateReturnUser } from "../models/mssqlModel";
 const SteamStrategy = require("passport-steam");
 
-export const steamStrategy =
-  new SteamStrategy(
-    {
-      returnURL: "http://localhost:3000/api/auth/callback", //TODO change this to work in production too
-      realm: "http://localhost:3000/", //TODO change this to work in production
-      apiKey: config.steamApiKey,
-    },
-    function (identifier: any, profile: any, done: any) {
-      // TODO use profile here
-      //process.nextTick(function () {
-        //profile.identifier = identifier;
-        return done(null, profile);
-      //});
+export const steamStrategy = new SteamStrategy(
+  {
+    returnURL: "http://localhost:3000/api/auth/callback", //TODO change this to work in production too
+    realm: "http://localhost:3000/", //TODO change this to work in production
+    apiKey: config.steamApiKey,
+  },
+  async function (_identifier: any, profile: SteamUser, done: any) {
+    try {
+      const dbUser = await steamCreateReturnUser(profile);
+
+      return done(null, dbUser);
+    } catch (error) {
+      console.error(error);
+      done(null, null);
     }
-  )
-
-
+    //process.nextTick(function () {
+    //profile.identifier = identifier;
+    //});
+  }
+);
