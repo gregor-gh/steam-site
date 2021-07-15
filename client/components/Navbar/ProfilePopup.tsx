fc740a42435fad15b28447551a58f46159564b33
Gregor Imrie <73136777+gregor-gh@users.noreply.github.com>
2021-07-15 20:57:18 +0100

    Added log out button and completed sign in button

--- client/components/Navbar/ProfilePopup.tsx
+++ client/components/Navbar/ProfilePopup.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { Button } from "../Button";
+import useStore from "../useStore";
 import "./Navbar.css";
 
 export const ProfilePopup: React.FunctionComponent<
@@ -8,12 +9,26 @@ export const ProfilePopup: React.FunctionComponent<
     HTMLDivElement
   > & { visible: boolean }
 > = ({ visible }) => {
+  const { isLoggedIn } = useStore((state) => state);
+
+  function logOut() {
+    fetch("/api/auth/log-out").then(() => window.location.reload());
+  }
+
   return visible ? (
     <div className="profile-popup">
-      <Button>Sign into Steam-Site</Button>
-      <Link to="/register">
-        <Button type="secondary">Register with Steam-Site</Button>
-      </Link>
+      {isLoggedIn ? (
+        <Button onClick={logOut}>Sign out of Steam-Site</Button>
+      ) : (
+        <>
+          <a href="/api/auth">
+            <Button>Sign into Steam-Site</Button>
+          </a>
+          <Link to="/register">
+            <Button type="secondary">Register with Steam-Site</Button>
+          </Link>
+        </>
+      )}
     </div>
   ) : null;
 };
