9089492e98fc9d71a1c89df23a245b5dfa3e81ec
Gregor <73136777+gregor-gh@users.noreply.github.com>
2021-09-23 22:27:50 +0100

    Download user's steam games at login

--- server/strategies/steamStrategy.ts
+++ server/strategies/steamStrategy.ts
@@ -1,5 +1,6 @@
 import config from "../config";
 import { steamCreateReturnUser } from "../models/mssqlModel";
+import { downloadUserSteamGames } from "../models/steamModel";
 const SteamStrategy = require("passport-steam");
 
 export const steamStrategy = new SteamStrategy(
@@ -11,7 +12,7 @@ export const steamStrategy = new SteamStrategy(
   async function (_identifier: any, profile: SteamUser, done: any) {
     try {
       const dbUser = await steamCreateReturnUser(profile);
-
+      downloadUserSteamGames(dbUser[0].steamId);
       return done(null, dbUser);
     } catch (error) {
       console.error(error);
