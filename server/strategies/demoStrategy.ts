d013f0f691ff80164ebcc16f6368542ba2fcf8a4
Gregor <73136777+gregor-gh@users.noreply.github.com>
2021-10-10 23:51:46 +0100

    Modified the demo user to use my own account

--- server/strategies/demoStrategy.ts
+++ server/strategies/demoStrategy.ts
@@ -11,7 +11,13 @@ export const demoStrategy = new LocalStrategy(async function (
 ) {
   const demoProfile: SteamUser = {
     displayName: "demo",
-    id: "76561197960435530",
+    id: "76561198044893617",
+    photos: [
+      {
+        value:
+          "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb.jpg",
+      },
+    ],
   };
   const dbUser = await createReturnSteamUser(demoProfile);
   await downloadUserSteamGames(dbUser.steamId);
