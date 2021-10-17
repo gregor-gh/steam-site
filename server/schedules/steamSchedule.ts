b0596ede178bb73a8f06c518b5985b84863ca308
Gregor <73136777+gregor-gh@users.noreply.github.com>
2021-10-18 00:01:54 +0100

    Added schedule for updating steam global achs

--- server/schedules/steamSchedule.ts
+++ server/schedules/steamSchedule.ts
@@ -1,8 +1,10 @@
 import schedule from "node-schedule";
-import { getAllGames } from "../models/steamModel";
+import { getAllGames, updateAllSteamGameGlobalAchs } from "../models/steamModel";
 
 export function runSteamSchedule() {
   schedule.scheduleJob("0 0 7 1-31 1-12 1-7", async () => {
     getAllGames();
+    // TODO add a schedule for updating all users' achievement data
+    updateAllSteamGameGlobalAchs();
   });
 }
