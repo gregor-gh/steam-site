e2ac33cbb0b944aa1ab8c854c8cb88db1c7de942
Gregor <73136777+gregor-gh@users.noreply.github.com>
2021-10-20 23:17:52 +0100

    Corrected casing on import to fix ts error

--- client/pages/app.tsx
+++ client/pages/app.tsx
@@ -1,13 +1,13 @@
 import "../styles/App.css";
 import { useEffect } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-import { Home } from "./Home/Home";
 import { Navbar } from "../components/Navbar/Navbar";
 import "./App.css";
 import { Register } from "./Register/Register";
 import useStore from "../components/useStore";
 import { DbSteamUser } from "../../@types/database";
 import Game from "./Game/GamePage";
+import { Home } from "./Home/home";
 
 const App = () => {
   const { setIsLoggedIn, setSteamProfile } = useStore((state) => state);
